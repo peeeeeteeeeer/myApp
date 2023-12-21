559d75448202ebda9fac54cc8ae1508fa5a277d8
Peter Paulovic <ppaulovic@ohpen.com>
2023-12-21 10:16:28 +0100

    initial commit

---
+++ myApp/src/main/java/com/myApp/SimpleLambda.java
@@ -0,0 +1,31 @@
+package com.myApp;
+import com.amazonaws.services.lambda.runtime.Context;
+import com.amazonaws.services.lambda.runtime.RequestHandler;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class SimpleLambda implements RequestHandler<Map<String, Object>, ApiGatewayResponse> {
+
+    @Override
+    public ApiGatewayResponse handleRequest(Map<String, Object> input, Context context) {
+        // Extracting information from the API Gateway input
+        String httpMethod = (String) input.get("httpMethod");
+        String path = (String) input.get("path");
+
+        // Handling different HTTP methods
+        if ("GET".equals(httpMethod)) {
+            // Responding to a GET request
+            Map<String, String> responseBody = new HashMap<>();
+            responseBody.put("message", "Hello! You requested the resource at " + path);
+
+            return new ApiGatewayResponse(200, responseBody);
+        } else {
+            // Responding with an error for unsupported methods
+            Map<String, String> responseBody = new HashMap<>();
+            responseBody.put("error", "Method " + httpMethod + " not supported");
+
+            return new ApiGatewayResponse(405, responseBody);
+        }
+    }
+}
