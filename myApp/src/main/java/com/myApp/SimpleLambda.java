65bd0950757aa67028faacaa3bf7565b0ae89b81
Peter Paulovic <ppaulovic@ohpen.com>
2024-01-15 11:08:20 +0100

    imports

--- myApp/src/main/java/com/myApp/SimpleLambda.java
+++ myApp/src/main/java/com/myApp/SimpleLambda.java
@@ -1,6 +1,8 @@
 package com.myApp;
 import com.amazonaws.services.lambda.runtime.Context;
 import com.amazonaws.services.lambda.runtime.RequestHandler;
+import com.amazonaws.services.lambda.runtime.events.APIGatewayProxyRequestEvent;
+import com.amazonaws.services.lambda.runtime.events.APIGatewayProxyResponseEvent;
 
 import java.util.HashMap;
 import java.util.Map;
