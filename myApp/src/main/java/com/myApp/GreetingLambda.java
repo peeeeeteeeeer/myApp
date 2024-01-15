57dbe37b62f08335272f8e853e00e3a564aa1b16
Peter Paulovic <ppaulovic@ohpen.com>
2024-01-15 16:48:51 +0100

    aa

--- myApp/src/main/java/com/myApp/GreetingLambda.java
+++ myApp/src/main/java/com/myApp/GreetingLambda.java
@@ -3,30 +3,13 @@ package com.myApp;
 import com.amazonaws.services.lambda.runtime.Context;
 import com.amazonaws.services.lambda.runtime.RequestHandler;
 
-import java.util.HashMap;
-import java.util.Map;
-
-public class GreetingLambda implements RequestHandler<Map<String, Object>, ApiGatewayResponse> {
+public class GreetingLambda implements RequestHandler<ApiGatewayRequest, ApiGatewayResponse> {
 
     @Override
-    public ApiGatewayResponse handleRequest(Map<String, Object> input, Context context) {
-        // Extracting information from the API Gateway input
-        String httpMethod = (String) input.get("httpMethod");
-        String path = (String) input.get("path");
+    public ApiGatewayResponse handleRequest(ApiGatewayRequest input, Context context) {
+        String name = input.getQueryStringParameters().getOrDefault("name", "Guest");
+        String greeting = "Hello, " + name + "!";
 
-        // Handling different HTTP methods
-        if ("GET".equals(httpMethod)) {
-            // Responding to a GET request
-            Map<String, String> responseBody = new HashMap<>();
-            responseBody.put("message", "Hello! You requested the resource at " + path);
-
-            return new ApiGatewayResponse(200, responseBody);
-        } else {
-            // Responding with an error for unsupported methods
-            Map<String, String> responseBody = new HashMap<>();
-            responseBody.put("error", "Method " + httpMethod + " not supported");
-
-            return new ApiGatewayResponse(405, responseBody);
-        }
+        return new ApiGatewayResponse(greeting, 200);
     }
 }
